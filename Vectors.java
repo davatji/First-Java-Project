9211d599921d02f9324ff762e4afe817c273183d
Dava Hannas Putra <146698831+davatji@users.noreply.github.com>
2024-01-25 16:55:48 +0700

    Create Vectors.java

---
+++ Vectors.java
@@ -0,0 +1,13 @@
+//namespace-like class
+public class Vectors {
+	
+	public static class Vector2Int {
+		public int x;
+		public int y;
+		
+		Vector2Int(int x, int y){
+			this.x = x;
+			this.y = y;
+		}
+	}
+}
