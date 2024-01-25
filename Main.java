8ec19b2017c0b7db8f9d983b10e2b4c0405cc5b8
Dava Hannas Putra <146698831+davatji@users.noreply.github.com>
2024-01-25 16:51:50 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main 
+{	
+	public static void main(String[] args) {
+		/*Running an initial interface preceding the game.
+		 * Done to allow users setting the parameters that influence how the game is run*/
+		new InitialInterface();
+	}
+}
